258c31e022422b7cc8364e6888f5a0cca243c8a5
Chris Humboldt <chrismodem@gmail.com>
2014-03-28 07:24:03 +0200

    Basic Update

--- js/penplate.js
+++ js/penplate.js
@@ -13,10 +13,14 @@
 	{	
 		// ------------------------------------------------ Setup
 		var $object 						= this;
+		var $this_penplate;
 		
 
 		// ------------------------------------------------ Settings
-		$object.settings 					= {};
+		$object.settings 					= 
+		{
+			breakpoint						: '700',
+		};
 		
 
 		// ------------------------------------------------ Initialize
@@ -36,7 +40,14 @@
 				$this_penplate.html('<p>'+ $this_penplate_content +'</p>');
 			}
 			$this_penplate.attr({ 'contenteditable' : 'true' });
-			// document.execCommand('defaultParagraphSeparator', false, 'p');
+			document.execCommand('defaultParagraphSeparator', false, 'p');
+
+			// Window type
+			$object.window_type();
+			$(window).resize(function()
+			{
+				$object.window_type();
+			});
 
 			// Penplate functions
 			$object.new_paragraph();
@@ -46,26 +57,79 @@
 		
 
 		// ------------------------------------------------ Functions
+		// Window type
+		$object.window_type					= function()
+		{
+			if($(window).width() <= $object.settings.breakpoint)
+			{
+				// Set the type variable
+				$('html').addClass('penplate-small-view');
+				$('html').removeClass('penplate-large-view');
+			}
+			else
+			{
+				// Set the type variable
+				$('html').removeClass('penplate-small-view');
+				$('html').addClass('penplate-large-view');
+			}
+		}
+
 		// Activate controls
 		$object.activate_controls			= function($this_penplate)
 		{
 			// Show
-			$this_penplate.on('dblclick', function()
+			$this_penplate.on('mouseup', function()
 			{
-				$object.show_controls('web');	
+				$object.check_selection();
+			});
+			$this_penplate.on('keyup', function()
+			{
+				$object.check_selection();
 			});
 
 			// Hide
-			$('html').on('click', function()
+			$('html').on('click', function($e)
 			{
-				$object.hide_controls();
+				// Some variables
+				var $target 				= $($e.target);
+				var $target_check 			= $target.parents('.penplate-controls').length;
+
+				// Hide the menu under condition
+				if($target_check == false)
+				{
+					$object.hide_controls();
+				}
+				$(window).resize(function()
+				{
+					$object.hide_controls();
+				});
 			});
 		}
-		$object.show_controls 				= function($type)
+
+		// Check selection
+		$object.check_selection 			= function()
 		{
-			// Attach class to HTML
-			if($('html').hasClass('show-penplate-controls') == false)
+			// Check if selection is empty
+			$selection 						= window.getSelection();
+
+			// Only show if range
+			if($selection.type == 'Range')
 			{
+				// Show the controls
+				$object.show_controls();
+			}
+		}
+
+		// Show controls
+		$object.show_controls 				= function()
+		{
+			// Position control			
+			$object.set_control_position();
+
+			// Show the controls
+			if($('html').hasClass('show-penplate-controls') == false)
+			{	
+				// Attach class to HTML
 				$('html').addClass('show-penplate-controls');
 			}
 
@@ -77,22 +141,39 @@
 				}
 			});
 		}
+
+		// Hide controls
 		$object.hide_controls 				= function()
 		{
 			// Remove class on HTML 
-			if($('html').hasClass('penplate-controls-open'))
+			if($('html').hasClass('penplate-'))
 			{
-				$('html').removeClass('show-penplate-controls');
-			}
-
-			$('.penplate-controls').bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function()
-			{
-				if($('html').hasClass('show-penplate-controls') == false)
+				if($('html').hasClass('penplate-controls-open'))
 				{
-					$('html').removeClass('penplate-controls-open');
+					$('html').removeClass('show-penplate-controls').removeClass('penplate-controls-open');
+					$('.penplate-controls').css({ top: '-100px' });
 				}
-			});
+			}
 		}
+
+		// Set controls position
+		$object.set_control_position 		= function()
+		{
+			// Some variables
+			$control_w 						= $('.penplate-controls').outerWidth();
+			$control_h 						= $('.penplate-controls').height();
+   			$selection 						= window.getSelection();
+   			$range 							= $selection.getRangeAt(0);
+   			$boundary						= $range.getBoundingClientRect();
+   			$boundary_center 				= ($boundary.left + $boundary.right) / 2;
+
+			// Some variables
+			$offset_left 					= $boundary_center - ($control_w / 2);
+			$offset_top						= $boundary.top + window.pageYOffset - ($control_h + 12);
+
+			// Set the position
+			$('.penplate-controls').css({ left: $offset_left, top: $offset_top });
+        }
 
 		// Edit current selection
 		$object.edit_selection				= function()
@@ -120,18 +201,39 @@
 				{
 					// Get parent tag type
 					$parent_tag_type 		= $object.get_parent_tag_type();
-					// console.log($parent_tag_type);
 
 					// Apply the format
 					if($parent_tag_type != $pen_edit_action)
 					{
-						document.execCommand('formatBlock', false, $pen_edit_action);
+						// Super parent
+						$super_parent 	= window.getSelection().anchorNode.parentElement.parentElement.nodeName.toLowerCase();
+
+						// Block quote check - Firefox issue with parent tag being p within blockquote
+						if($pen_edit_action == 'blockquote')
+						{
+			                document.execCommand('outdent', false, null);
+
+			                if($super_parent != 'blockquote'){
+								document.execCommand('formatBlock', false, $pen_edit_action);
+							}
+			            }
+			            else
+			            {
+			            	document.execCommand('formatBlock', false, $pen_edit_action);
+			            }
 					}
 					else
 					{
 						document.execCommand('formatBlock', false, 'p');
 					}
 				}
+
+				// Reposition controls if need be
+				setTimeout(function()
+		    	{
+					$object.set_control_position();
+		    	},
+		    	30);
 			});
 		}
 
@@ -156,7 +258,7 @@
 			    	{
 			    		document.execCommand('formatBlock', false, 'p');
 			    	},
-			    	30);
+			    	10);
 			    }
 			});
 		}
